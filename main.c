d67cf7f3a63ce06a562f21ce76619aa0ccf51ee1
Zachary Yedidia <zyedidia@gmail.com>
2025-09-10 12:01:53 -0700

    Update main

--- main.c
+++ main.c
@@ -2,7 +2,10 @@
 #include <dlfcn.h>
 int main() {
     void *handle = dlopen("./libfoo.so", RTLD_NOW);
-    printf("done dlopen\n");
+    if (!handle) {
+        printf("dlopen failed\n");
+        return 0;
+    }
     printf("loaded: %p\n", handle);
     void *sym = dlsym(handle, "add");
     printf("sym: %p\n", sym);
